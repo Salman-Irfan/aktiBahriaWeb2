66dd36a94ef6d124b4f6ba64b0c373a4ccbdcb8a
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2024-04-28 15:57:46 +0500

    dom selectors

---
+++ week6/domSelectors.js
@@ -0,0 +1,14 @@
+const roomId = document.getElementById("roomId")
+console.log(roomId)
+
+const bgRed = document.getElementsByClassName("bgRed")
+console.log(bgRed[1])
+
+const div = document.getElementsByTagName("div")
+console.log(div)
+
+// querySelector
+console.log(`query selctors`)
+const idSelector = document.querySelectorAll("div")
+console.log(idSelector)
+// query selector all
