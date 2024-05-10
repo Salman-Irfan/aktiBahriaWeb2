c7920f1deefe62ee608ee7ca76d004e034513afc
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2024-05-10 23:09:02 +0500

    server json response

--- week7/dummyApi.js
+++ week7/dummyApi.js
@@ -1,2 +1,21 @@
-const response = axios.get(`https://jsonplaceholder.typicode.com/todos/1`)
-console.log(response)
+const displayApiData = async () => {
+    try {
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/todos/1`)
+        console.log(response)
+    } catch (error) {
+        console.log(error.message)
+        if(error.message){
+            alert(`service unavailable for some technical reason`)
+        }
+    }
+}
+displayApiData()
+
+
+console.log(123)
+
+const checkServer = document.getElementById("checkServer")
+
+checkServer.innerHTML = `
+    <p>changed through js client side side</p>
+`
--- week7Day2nodeJs/index.js
+++ week7Day2nodeJs/index.js
@@ -9,6 +9,27 @@ app.get('/about', (req, res) => {
     res.send('<h1>hello about page from from nodemon</h1>')
 })
 
+
+
+app.get(`/test-get-json`, (req, res)=>{
+    res.json({
+        success: true,
+        data: 123,
+        age: 25,
+        name: "node server"
+    })
+})
+
+// http get
+app.get(`/todos/1`, (req, res)=>{
+    res.json({
+        userId: 1,
+        id: 1,
+        title: "koi ajeeb c english",
+        completed: false
+    })
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on ports ${port}`)
 })
